934ffc715f3787e7ae75073e3ea7308d3f089875
alladavletshina <124310142+alladavletshina@users.noreply.github.com>
2025-12-06 15:04:28 +0300

    Update logger.go

--- utils/logger.go
+++ utils/logger.go
@@ -10,12 +10,10 @@ func LogUserAction(action string, userID int) {
     logMessage := fmt.Sprintf("[%s] Action: %s, UserID: %d", 
         time.Now().Format(time.RFC3339), action, userID)
     
-    // В реальном приложении здесь можно писать в файл или отправлять в ELK
     log.Println(logMessage)
     
-    // Имитация асинхронной работы
     go func(msg string) {
-        time.Sleep(100 * time.Millisecond) // имитация задержки
+        time.Sleep(100 * time.Millisecond) 
         fmt.Printf("Async log completed: %s\n", msg)
     }(logMessage)
 }
